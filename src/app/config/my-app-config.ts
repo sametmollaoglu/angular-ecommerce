913d25746947302552d4f39a99c2449f5372d245
samet <samet.mollaoglu85@gmail.com>
2024-03-04 11:39:13 +0300

    secure communication with HTTPS fe - 280

--- src/app/config/my-app-config.ts
+++ src/app/config/my-app-config.ts
@@ -2,7 +2,7 @@ export default {
   oidc: {
     clientId: '0oaffzx09rcc0HClC5d7',
     issuer: 'https://dev-71739810.okta.com/oauth2/default',
-    redirectUri: 'http://localhost:4200/login/callback',
+    redirectUri: 'https://localhost:4200/login/callback',
     scopes: ['openid', 'profile', 'email'],
   },
 };
