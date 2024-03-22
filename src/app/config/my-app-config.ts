c39b641f559305e4ac7f52c387c1c3e6c5a3b0a1
samet <samet.mollaoglu85@gmail.com>
2024-03-22 15:36:57 +0300

    tomcat configurations

--- src/app/config/my-app-config.ts
+++ src/app/config/my-app-config.ts
@@ -2,7 +2,7 @@ export default {
   oidc: {
     clientId: '0oaffzx09rcc0HClC5d7',
     issuer: 'https://dev-71739810.okta.com/oauth2/default',
-    redirectUri: 'https://localhost:4200/login/callback',
+    redirectUri: 'http://localhost:8080/angular-ecommerce/login/callback',
     scopes: ['openid', 'profile', 'email'],
   },
 };
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  luv2shopApiUrl: 'http://localhost:8080/spring-boot-ecommerce/api',
+  stripePublishableKey:
+    'pk_test_51OqvPsCd2J8Xuty4GfFvQV6uC8ZskiJll9K7hcQ2bgLShImf6l20KYASduJNQXwbzPTGcjTlmbrVthiKmMtdCte1004moXSFeF',
 };
